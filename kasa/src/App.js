0fc09cd336ae251ea9a6ab347af7667c46cab427
Kayangray <kayangray@gmail.com>
2023-05-30 14:43:20 +0200

    push final ?

--- kasa/src/App.js
+++ kasa/src/App.js
@@ -1,9 +1,9 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home.jsx";
-import About from "./pages/About/About.jsx";
-import Header from "./components/Header/Header.jsx";
-import FicheLogement from "./pages/FicheLogement/FicheLogement.jsx";
-import Footer from "./components/Footer/Footer.jsx";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Header from "./components/Header/Header";
+import FicheLogement from "./pages/FicheLogement/FicheLogement";
+import Footer from "./components/Footer/Footer";
 import Error from "./pages/Error/Error.jsx";
 
 function App() {
