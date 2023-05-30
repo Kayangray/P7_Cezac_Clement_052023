63fd99fbe885fc27d0c5261da2a196f9fb016091
Kayangray <kayangray@gmail.com>
2023-05-30 09:20:09 +0200

    push

---
+++ kasa/src/components/AboutBanner/AboutBanner.jsx
@@ -0,0 +1,4 @@
+//les éléments sont une image et un calque assombrissant qui varient selon le responsive
+export default function AboutBanner() {
+	return <div className="aboutBackground wh-msk"></div>;
+}
