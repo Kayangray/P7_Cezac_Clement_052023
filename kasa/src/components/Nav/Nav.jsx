2b32e3882e5307ed3fbda5df053255e9a2a1fc8e
Kayangray <kayangray@gmail.com>
2023-06-15 10:18:06 +0200

    nav sousligné pour page actuel

--- kasa/src/components/Nav/Nav.jsx
+++ kasa/src/components/Nav/Nav.jsx
@@ -1,14 +1,14 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Nav() {
 	return (
 		<nav className="nav-header">
-			<Link to="/" className="nav-header_link-home">
+			<NavLink to="/" className="nav-header_link-home">
 				Accueil
-			</Link>
-			<Link to="/about" className="nav-header_link-about">
+			</NavLink>
+			<NavLink to="/about" className="nav-header_link-about">
 				A Propos
-			</Link>
+			</NavLink>
 		</nav>
 	);
 }
